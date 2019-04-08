5ce8288f9d2e1f3311193259c15b914528036007
SergKorol <korol_sergey@me.com>
2019-04-08 16:45:10 +0300

    save

---
+++ ClientApp/src/app/login/login.externalproviders.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login.externalproviders',
+  templateUrl: './login.externalproviders.component.html',
+  styleUrls: ['./login.externalproviders.component.css']
+})
+export class Login.ExternalprovidersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
